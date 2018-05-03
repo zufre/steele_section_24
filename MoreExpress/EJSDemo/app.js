2fc7c2300fcd16ee2e445127c93f680275073cc3
User <user@Mac.local>
2018-05-03 22:20:56 +1000

    updated stylesheet and included partials

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static("public"));
+app.set('view engine', 'ejs')
 app.get('/', function(req, res) {
 
-  res.render('home.js')
+  res.render('home')
 });
 
 app.get('/fallinginlovewith/:thing', function(req, res) {
   var thing = req.params.thing;
 
-  res.render('love.ejs', {thingVar: thing})
+  res.render('love', {thingVar: thing})
 });
 
 app.get('/posts', function(req, res) {
@@ -21,7 +23,7 @@ app.get('/posts', function(req, res) {
 
   ];
 
-  res.render('posts.ejs', {posts: posts})
+  res.render('posts', {posts: posts})
 });
 
 app.listen(3000, process.env.IP, function(){
