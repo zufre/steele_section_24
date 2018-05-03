6d12404842f23dff6900e3a8c860571bfea80a3c
User <user@Mac.local>
2018-05-03 13:42:31 +1000

    added EJESDEmo

---
+++ MoreExpress/EJSDemo/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/fallinginlovewith/:thing', function(req, res) {
+  var thing = req.params.thing;
+
+  res.render('love.ejs', {thingVar: thing})
+});
+
+app.get('/posts', function(req, res) {
+  var posts =  [
+
+    {title: "Post 1", author: "James"},
+    {title: "Post 2", author: "Susy"},
+    {title: "Post 3", author: "Susy"}
+
+  ];
+
+  res.render('posts.ejs', {posts: posts})
+});
+
+app.listen(3000, process.env.IP, function(){
+  console.log('server running');
+});
